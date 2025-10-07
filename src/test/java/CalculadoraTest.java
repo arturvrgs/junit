41ebbe99811f3cda85f6968b04e39f3e4deef7fd
Artur Vargas <arturvargaslg@gmail.com>
2025-10-07 11:47:44 -0300

    add more assertions

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalculadoraTest {
 
     @Test
@@ -10,5 +13,28 @@ public class CalculadoraTest {
 
         Assertions.assertTrue(calc.soma(2, 3) == 5);
         Assertions.assertEquals(5, calc.soma(2, 3));
+    }
+
+    @Test
+    public void assertivas() {
+        Assertions.assertEquals("Casa", "Casa");
+        Assertions.assertNotEquals("Casa", "casa");
+        Assertions.assertTrue("casa".equalsIgnoreCase("CASA"));
+        Assertions.assertTrue("Casa".endsWith("sa"));
+        Assertions.assertTrue("Casa".startsWith("Ca"));
+
+        List<String> s1 = new ArrayList<>();
+        //s1.add("a"); daria erro
+        List<String> s2 = new ArrayList<>();
+        List<String> s3 = null;
+
+        Assertions.assertEquals(s1, s2);
+
+        //compara referencias de memoria
+        Assertions.assertSame(s1, s1);
+
+        //verifica null
+        Assertions.assertNull(s3);
+        Assertions.assertNotNull(s1);
     }
 }
